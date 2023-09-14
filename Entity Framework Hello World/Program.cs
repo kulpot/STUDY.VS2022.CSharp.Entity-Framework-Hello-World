ca5b4ccdefd494ba338279e4851d4d4de101b6d4
Kulpot <kulpotwahu@gmail.com>
2023-09-14 20:06:18 +0800

    Add project files.

---
+++ Entity Framework Hello World/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Data.Entity;
+
+//ref link:https://www.youtube.com/watch?v=v2tO15ctPyY&list=PLRwVmtr-pp06bXl6mbwDfK1eW9sAIvWUZ
+//
+
+class Video
+{
+    public string Title { get; set; }
+    public string Description { get; set; }
+}
+
+class MeContext : DbContext
+{
+    public MeContext() : base(@"")
+    {
+        
+    }
+}
+
+class MainClass
+{
+    static void Main()
+    {
+        var vid = new Video
+        {
+            Title = "Hello World Entity Framework",
+            Description = "Learn about the entity framework"
+        };
+    }
+}
